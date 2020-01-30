0e63b2463697e8a1e5e62dc9c0e0724d40515ad1
Anthony J. Clark <ajc@>
2020-01-30 10:53:15 -0600

    Added enums example.

---
+++ 2020-01-30_rust-examples/enums.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+enum StringOrInteger {
+    S(String),
+    I(i32),
+}
+
+fn function_that_returns_string_or_integer(x: i32) -> StringOrInteger {
+    if x > 5 {
+        StringOrInteger::S("one".to_string())
+    } else {
+        StringOrInteger::I(1)
+    }
+}
+
+fn main() {
+    let x_string_or_int = function_that_returns_string_or_integer(5);
+
+    dbg!(x_string_or_int);
+}
